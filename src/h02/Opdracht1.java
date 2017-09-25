ced3b6b1efcd74b342a366e75d7ea1dcd2126d0f
yarileewens <yleewens@roc-dev.com>
2017-09-25 11:42:59 +0200

    Begin gemaakt met hoofdstuk 2

---
+++ src/h02/Opdracht1.java
@@ -0,0 +1,17 @@
+package h02;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class Opdracht1 extends Applet {
+
+    @Override
+    public void init() {
+        super.init();
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+    }
+}
